f4ecbd91bc293743ab9431a8fa55724ee939304a
jklpdgoat <jarpadul@gmail.com>
2023-01-13 00:13:39 +0800

    Try Oxalica's overlay

---
+++ integrate_svelte_app_rust_wasm/old_shell.nix
@@ -0,0 +1,24 @@
+# Dead simple dev shell environment
+let
+  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
+  # pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
+
+  # Rolling updates, not deterministic.
+  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rust-analyzer
+
+    pkgs.nodejs
+    pkgs.wasm-pack
+    pkgs.nodePackages_latest.svelte-language-server
+    pkgs.nodePackages_latest.typescript-language-server
+  ];
+
+  # RUSTFLAGS var to set a path for the linker to recognize
+  shellHook = ''
+    export RUSTFLAGS="-L /home/lcb/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/wasm32-unknown-unknown/lib/"
+  '';
+}
