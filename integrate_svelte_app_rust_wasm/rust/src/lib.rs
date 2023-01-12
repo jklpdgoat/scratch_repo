29d6ec68c7858de5cee0e83b07eb48c7f8eef2c3
jklpdgoat <jarpadul@gmail.com>
2023-01-12 22:07:25 +0800

    Update rust dependencies

--- integrate_svelte_app_rust_wasm/rust/src/lib.rs
+++ integrate_svelte_app_rust_wasm/rust/src/lib.rs
@@ -1,14 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
