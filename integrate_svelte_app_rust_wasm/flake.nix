1f7dfda24e5809c62d51eeb96db212bc54290f08
jklpdgoat <jarpadul@gmail.com>
2023-01-13 00:55:40 +0800

    Add the correct linker path to wasm32-unknown-unknown target

--- integrate_svelte_app_rust_wasm/flake.nix
+++ integrate_svelte_app_rust_wasm/flake.nix
@@ -34,6 +34,7 @@
           shellHook = ''
             alias ls=exa
             alias find=fd
+            export RUSTFLAGS="-L /home/lcb/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/wasm32-unknown-unknown/lib/"
           '';
         };
       }
