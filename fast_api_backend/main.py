5b234c032028cc59339a0096022236993e1a07f8
jklpdgoat <jarpadul@gmail.com>
2023-01-27 23:21:38 +0800

    Declare Request Data

--- fast_api_backend/main.py
+++ fast_api_backend/main.py
@@ -1,8 +1,8 @@
-# from enum import Enum
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
@@ -10,20 +10,18 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
 
 @app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Item,
-    user: User,
-    importance: int = Body(gt=0),
-    q: str | None = None
-):
-    results = {"item_id": item_id, "item": item, "user": user, "impotance": importance}
-    if q:
-        results.update({"q": q})
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
