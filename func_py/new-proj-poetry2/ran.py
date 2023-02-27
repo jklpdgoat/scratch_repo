94bfa3d75087f897346020b13fe4a00d9e7a8a28
jklpdgoat <jarpadul@gmail.com>
2023-02-28 03:13:38 +0800

    Testing python modules

---
+++ func_py/new-proj-poetry2/ran.py
@@ -0,0 +1,14 @@
+import random
+
+rand1 = random.randint(1, 100)
+rand2 = random.randint(1, 100)
+
+ranrange1 = random.randrange(1, 10)
+ranrange2 = random.randrange(1, 10, 2)
+ranrange3 = random.randrange(0, 101, 10)
+
+print(rand1)
+print(rand2)
+print(ranrange1)
+print(ranrange2)
+print(ranrange3)
