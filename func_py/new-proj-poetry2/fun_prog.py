c76c81b3df0c65c908f0c397b7706d788b519638
jklpdgoat <jarpadul@gmail.com>
2023-03-04 22:23:22 +0800

    More fun fun fun:

--- func_py/new-proj-poetry2/fun_prog.py
+++ func_py/new-proj-poetry2/fun_prog.py
@@ -1,19 +1,21 @@
-list(map(int, ["1", "2", "3"]))
+from functools import reduce
 
-def hello_world(h):
-    def world(w):
-        print(h, w)
-    return world # Returning functions
+# define a function `call` where you provide the function and the arguments
+def call(x, f):
+    return f(x)
 
-h = hello_world
-x = h("hello")
-print(x)
-print(x("world"))
+# define a function that returns the square
+square = lambda x : x * x
 
-def naive_sum(list):
-    s = 0
-    for l in list:
-        s += 1
-    return s
+# define a function that returns the increment
+increment = lambda x : x + 1
 
-print(naive_sum)
+# define a function that returns the cube
+cube = lambda x : x * x * x
+
+# define a function that returns the decrement
+decrement = lambda x : x - 1
+
+funcs = [square, increment, cube, decrement]
+# funcs = [square]
+print(reduce(call, funcs, 96))
