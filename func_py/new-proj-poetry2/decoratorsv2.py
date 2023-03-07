e446b716c7155d4cbe0e2152140c43c05669cbff
jklpdgoat <jarpadul@gmail.com>
2023-03-08 00:40:06 +0800

    Update decor

--- func_py/new-proj-poetry2/decoratorsv2.py
+++ func_py/new-proj-poetry2/decoratorsv2.py
@@ -30,3 +30,15 @@ def parentv2(num):
         return first_child
     else:
         return second_child
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happning before the functionn is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+def say_whee():
+    print("Whee!")
+
+say_whee = my_decorator(say_whee)
