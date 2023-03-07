b56a08824b33a7d56512e3b3a70d1f005cf39cf5
jklpdgoat <jarpadul@gmail.com>
2023-03-07 23:48:35 +0800

    Decor v2 from dev branch

---
+++ func_py/new-proj-poetry2/decoratorsv2.py
@@ -0,0 +1,20 @@
+def say_hello(name):
+    return f"Hello {name}"
+
+def be_awesome(name):
+    return f"Yo {name}, together we are the awesomest!"
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
+
+def parent():
+    print("Printing from the parent() function")
+
+    def first_child():
+        print("Printing from the first_child() function")
+
+    def second_child():
+        print("Printing from the second_child() function")
+
+    second_child()
+    first_child()
